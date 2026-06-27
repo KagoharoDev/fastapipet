07d49d16ea5e62d05e5cbc3da424ab983d7c0e6e
Aleksandr Kagoharo <kagoharo.ito@gmail.com>
2026-06-27 17:13:09 +0400

    Base: Added requirements.txt, updated .gitignore, deleted obvious comments

--- backend/app/core/dependencies.py
+++ backend/app/core/dependencies.py
@@ -8,25 +8,18 @@ from app.db.session import SessionLocal
 import logging
 
 logger = logging.getLogger(__name__)
-# async def get_db() -> AsyncGenerator[AsyncSession, None]:
-#     async with SessionLocal() as session:
-#         yield session
-
-
-# DBDependency = Annotated[AsyncSession, Depends(get_db)]
-
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with SessionLocal() as session:
         try:
             yield session
-            await session.commit()  # Commit when all goes well
+            await session.commit()
         except Exception as e:
-            await session.rollback()  # Rollback on any error
+            await session.rollback()
             logger.exception(f"Database transaction rolled back: {e}")
             raise
         finally:
-            await session.close()  # Ensure session is closed
+            await session.close()
 
 
 DBDependency = Annotated[AsyncSession, Depends(get_db)]
